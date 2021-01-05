fc175a900175d49f349984aa81112e4055a0c184
ALEXANDRU-GEORGE RETEGAN <alexandru.retegan@stud.ubbcluj.ro>
2021-01-05 21:48:17 +0200

    Add project files.

---
+++ Controllers/ShopListsController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Retegan_Alexandru_L12.Data;
+using Retegan_Alexandru_L12.Models;
+
+namespace Retegan_Alexandru_L12.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShopListsController : ControllerBase
+    {
+        private readonly Retegan_Alexandru_L12Context _context;
+
+        public ShopListsController(Retegan_Alexandru_L12Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ShopLists
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ShopList>>> GetShopList()
+        {
+            return await _context.ShopList.ToListAsync();
+        }
+
+        // GET: api/ShopLists/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ShopList>> GetShopList(int id)
+        {
+            var shopList = await _context.ShopList.FindAsync(id);
+
+            if (shopList == null)
+            {
+                return NotFound();
+            }
+
+            return shopList;
+        }
+
+        // PUT: api/ShopLists/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutShopList(int id, ShopList shopList)
+        {
+            if (id != shopList.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(shopList).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ShopListExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ShopLists
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<ShopList>> PostShopList(ShopList shopList)
+        {
+            _context.ShopList.Add(shopList);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetShopList", new { id = shopList.ID }, shopList);
+        }
+
+        // DELETE: api/ShopLists/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ShopList>> DeleteShopList(int id)
+        {
+            var shopList = await _context.ShopList.FindAsync(id);
+            if (shopList == null)
+            {
+                return NotFound();
+            }
+
+            _context.ShopList.Remove(shopList);
+            await _context.SaveChangesAsync();
+
+            return shopList;
+        }
+
+        private bool ShopListExists(int id)
+        {
+            return _context.ShopList.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ Data/Retegan_Alexandru_L12Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Retegan_Alexandru_L12.Models;
+
+namespace Retegan_Alexandru_L12.Data
+{
+    public class Retegan_Alexandru_L12Context : DbContext
+    {
+        public Retegan_Alexandru_L12Context (DbContextOptions<Retegan_Alexandru_L12Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Retegan_Alexandru_L12.Models.ShopList> ShopList { get; set; }
+    }
+}
